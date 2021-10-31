35c905544a8fdf98f0b7636cbf79d615657511fa
shreyank.sharma <sharma.shreyank6@gmail.com>
2021-10-31 16:16:47 +0530

    error commit

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-val1 = 10
+val1 = 10 
 val2 = 2
 val3 = 3
 
@@ -8,6 +8,9 @@ mul = val1 * val2
 div = val1 / val2
 
 print("Sum:", sum)
-print("Sub:", sub)
+    print("Sub:", sub)
 print("Mul:", mul)
 print("Div:", div)
+
+for i in range(1,11):
+print(i)
