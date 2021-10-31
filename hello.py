6f9482ac8669e80599cacedd50271abff66d1957
shreyank.sharma <sharma.shreyank6@gmail.com>
2021-10-31 16:13:56 +0530

    syntax checker

---
+++ hello.py
@@ -0,0 +1,13 @@
+val1 = 10
+val2 = 2
+val3 = 3
+
+sum = val1 + val2
+sub = val1 - val2
+mul = val1 * val2
+div = val1 / val2
+
+print("Sum:", sum)
+print("Sub:", sub)
+print("Mul:", mul)
+print("Div:", div)
